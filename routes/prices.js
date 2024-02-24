f8b846f9bc5d142ff9b5d4df883618b127bb6246
Mostafa AbdElmogod <mostafa-for-work@hotmail.com>
2024-02-24 02:28:02 +0200

    ed

---
+++ routes/prices.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const Cardlib= require('../model/cardlib');
+const CardAca= require('../model/cardaca');
+const CardBuy= require('../model/cardbuy');
+
+/* GET home page. */
+router.get('/', async(req, res, next) => {
+  const cardlib = await Cardlib.findOne().sort({ _id: -1 }).limit(1)
+  const Cardaca = await CardAca.findOne().sort({ _id: -1 }).limit(1)
+  const Cardbuy = await CardBuy.findOne().sort({ _id: -1 }).limit(1)
+  res.render('prices', { cardlib: cardlib ,cardaca:Cardaca,cardbuy:Cardbuy});
+});
+
+module.exports = router;
